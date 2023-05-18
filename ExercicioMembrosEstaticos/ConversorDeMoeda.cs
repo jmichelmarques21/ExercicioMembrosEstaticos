680c84bfea70e6cee430d6889e61d276d28cf82d
Jean Marques <jmichelmarques21@outlook.com>
2023-05-17 22:06:58 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioMembrosEstaticos {
+    internal class ConversorDeMoeda {
+
+        public static double Conversor(double cot, double valor) {
+            double valorReal = cot * valor;
+            valorReal += valorReal * 0.06;
+            return valorReal;
+        }
+
+    }
+}
---
+++ ExercicioMembrosEstaticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ExercicioMembrosEstaticos {
+    class Program {
+        static void Main(string[] args) {
+
+
+            /* Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa
+               em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar
+               6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos.*/
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quant = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais: R$" + ConversorDeMoeda.Conversor(cotDolar, quant).ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
